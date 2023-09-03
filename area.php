5b2ef941fff7643be96535fe533ab0d34cb1aa6d
Diana-Kaba <kabadiana327@gmail.com>
2023-09-03 19:35:31 +0300

    first commit

---
+++ area.php
@@ -0,0 +1,25 @@
+<?php
+$r = rand(5, 500);
+$s = round(M_PI * $r ** 2);
+
+$start = <<<EOD
+<!DOCTYPE html>
+<html lang="uk">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Площа кола</title>
+</head>
+<body>
+<h2>3.3 Площа кола</h2>
+<p>Площа - <b>$s</b></p>
+EOD;
+echo $start;
+
+echo "<div style=\"background-color: lightblue; border-radius: 50%; width: ".(2 * $r)."px; height: ".(2 * $r)."px;\"></div>";
+
+$end = <<<EOD
+</body>
+</html>
+EOD;
+echo $end;
